25d6700abc6bc24090d3a43e3c32444919ef648f
jamill hallack <jnbezhal@gmail.com>
2024-07-29 17:02:22 +0400

    feat: Add Ethereum utility functions for address and message hash generation
    - Implement GetAddress to derive the Ethereum address from an ECDSA public key.
    - Implement CreateMessageHash to generate a Keccak-256 hash of the message parameters.
    - Create Ethereum signed message hash by prefixing with Ethereum's standard message prefix and re-hashing.
    
    This commit provides essential utility functions for generating Ethereum addresses and message hashes, facilitating the creation of signed messages compliant with Ethereum standards.

---
+++ ethereum/eth_utils.go
@@ -0,0 +1,36 @@
+package ethereum
+
+import (
+	"crypto/ecdsa"
+	"crypto/elliptic"
+	"encoding/binary"
+	"encoding/hex"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/crypto"
+	"math/big"
+)
+
+func GetAddress(pubKey *ecdsa.PublicKey) common.Address {
+	pubBytes := elliptic.Marshal(pubKey.Curve, pubKey.X, pubKey.Y)
+	hash := crypto.Keccak256Hash(pubBytes[1:]).Bytes() // Exclude the prefix 0x04
+	return common.BytesToAddress(hash[12:])
+}
+
+func CreateMessageHash(authorizedSigner common.Address, landId uint64, founder common.Address, nonce string) ([32]byte, [32]byte, error) {
+	authorizedSignerBytes := authorizedSigner.Bytes()
+	landIdBytes := make([]byte, 32)
+	binary.BigEndian.PutUint64(landIdBytes[24:], landId)
+	founderBytes := founder.Bytes()
+	nonceBytes, err := hex.DecodeString(nonce)
+	if err != nil {
+		return [32]byte{}, [32]byte{}, err
+	}
+
+	message := append(authorizedSignerBytes, append(landIdBytes, append(founderBytes, nonceBytes...)...)...)
+	messageHash := crypto.Keccak256Hash(message)
+	prefix := []byte("\x19Ethereum Signed Message:\n32")
+	prefixedMessage := append(prefix, messageHash.Bytes()...)
+	ethSignedMessageHash := crypto.Keccak256Hash(prefixedMessage)
+
+	return messageHash, ethSignedMessageHash, nil
+}
